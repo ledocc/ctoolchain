124e2d828f944dea1f3a0b634a1353204dbc2cf6
David Callu <callu.david@gmail.com>
2019-04-12 14:29:55 +0200

    feat(toolchain): add msvc v141 and v142 toolset support

--- ctoolchain.cmake
+++ ctoolchain.cmake
@@ -4,5 +4,6 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/use_compiler.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/use_linker.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/use_toolset.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/os/use_os.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/use_flags.cmake")
